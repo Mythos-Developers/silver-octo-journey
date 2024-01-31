be4ac1bd7e0b6760c6f0a13389df4f021cc534ee
ScienceMind11 <sciencemind@icloud.com>
2024-01-30 20:51:03 -0500

    Update boilerplate code

---
+++ src/main/java/io/ybouzl/chaosblade/Chaosblade.java
@@ -0,0 +1,21 @@
+package io.ybouzl.chaosblade;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Chaosblade implements ModInitializer {
+
+	public static final String NAME = "Chaosblade";
+	public static final String ID = "chaosblade";
+	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+
+
+	}
+
+}
--- src/main/java/io/ybouzl/chaosblade/mixin/TitleScreenMixin.java
+++ src/main/java/io/ybouzl/chaosblade/mixin/TitleScreenMixin.java
@@ -1,6 +1,5 @@
-package com.example.example_mod.mixin;
+package io.ybouzl.chaosblade.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +8,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
+
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+
 	}
+
 }
